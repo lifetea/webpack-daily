660d536cd412969f69134d8cd7d4bee567fad761
lifetea <494886251@qq.com>
2016-11-17 23:00:10 +0800

    fixs #2 多个Js构建

---
+++ demo02/webpack.config.js
@@ -0,0 +1,14 @@
+/**
+ * Created by lifetea on 2016/11/17.
+ * email: 494886251@qq.com
+ * github: https://github.com/lifetea
+ */
+module.exports = {
+    entry:{
+        bundle1:'./bundle-1.js',
+        bundle2:'./bundle-2.js'
+    },
+    output:{
+        filename:'[name].js'
+    }
+};
